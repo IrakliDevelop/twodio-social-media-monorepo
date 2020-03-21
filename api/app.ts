a893c39cc0bc1617e4385e5254101140373ab0b4
Zurab Benashvili <zura.benashvili@gmail.com>
2020-03-21 17:01:59 +0400

    when running outside of serverless, start express manually.

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,14 @@
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 
+import config from './config';
+
 const app: Express = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+if (!config.isLambda) {
+    app.listen(config.server.port, () => console.log(`Api listening on port ${config.server.port}`));
+}
 
 export = app;
--- api/config/index.ts
+++ api/config/index.ts
@@ -4,6 +4,7 @@ import { config } from 'dotenv';
 config({ path: resolve(__dirname, '../.env' )});
 
 export = {
+    isLambda: !!(process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV),
     server: {
         port: process.env.SERVER_PORT || 3000,
         env: process.env.SERVER_ENV || 'development', // production, development
