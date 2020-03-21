ff6905aea2ab99cf7bda0b9ede0dea7e86915513
Irakli <i_iremashvili@cu.edu.ge>
2020-03-21 14:52:26 +0400

    add and initialize express and bodyParser

---
+++ api/app.ts
@@ -0,0 +1,6 @@
+import { express } from 'express';
+import { bodyParser } from 'body-parser';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
