4d4c8bf2615829255637efb630aa80a0f09b8ac8
Irakli <i_iremashvili@cu.edu.ge>
2020-03-27 00:35:21 +0400

    add verification handler

--- twodio-social-media-front/src/app/modules/login/sign-up/sign-up.component.ts
+++ twodio-social-media-front/src/app/modules/login/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {BehaviorSubject, Subject} from 'rxjs';
-import {takeUntil} from 'rxjs/operators';
+import {finalize, first, takeUntil} from 'rxjs/operators';
 import {Router} from '@angular/router';
 
 import { AuthService } from '@core/services';
@@ -13,6 +13,8 @@ import { AuthService } from '@core/services';
 })
 export class SignUpComponent implements OnInit, OnDestroy {
   signUpForm: FormGroup;
+  emailConfirmationRequired = false;
+  loading: boolean;
   error$: BehaviorSubject<any | null>;
   unsubscribe$: Subject<void>;
   constructor(
@@ -26,8 +28,9 @@ export class SignUpComponent implements OnInit, OnDestroy {
     this.error$ = new BehaviorSubject<any|null>(null);
     this.signUpForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      repeatPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      repeatPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      code: [''],
     });
   }
 
@@ -37,20 +40,54 @@ export class SignUpComponent implements OnInit, OnDestroy {
       // TODO: handle validation
     }
     const { email, password, repeatPassword } = this.signUpForm.value;
-    console.log(email, password, repeatPassword);
     if (password !== repeatPassword) {
       this.error$.next('passwords don\'t match');
       return;
     }
+    this.loading = true;
     this.authService.signUp(email, password ).pipe(
-      takeUntil(this.unsubscribe$)
+      takeUntil(this.unsubscribe$),
+      finalize(() => this.loading = false)
     ).subscribe(data => {
       console.log(data);
+      this.emailConfirmationRequired = true;
     }, err => {
       console.error(err);
       this.error$.next(err);
     });
   }
+
+  confirmClick() {
+    const { email, password, code } = this.signUpForm.getRawValue();
+    this.cognitoConfirmSignUp(email, password, code);
+  }
+
+  cognitoConfirmSignUp(email: string, password: string, code: string) {
+    this.loading = true;
+    this.authService.confirmSignUp(email, code)
+      .pipe(first())
+      .subscribe(() => {
+        this.signInAfterRegistration(email, password);
+      }, err => {
+        this.error$.next(err);
+        this.loading = false;
+      });
+  }
+  signInAfterRegistration(email: string, password: string) {
+    this.loading = true;
+
+    this.authService.signIn(email, password)
+      .pipe(first(),
+      finalize(() => this.loading = false)
+      ).subscribe(() => {
+        this.router.navigate(['/login']);
+      }, err => {
+        console.log(err);
+        this.error$.next(err);
+        this.router.navigate(['/login']);
+      });
+  }
+
 
   ngOnDestroy(): void {
     this.unsubscribe$.next();
