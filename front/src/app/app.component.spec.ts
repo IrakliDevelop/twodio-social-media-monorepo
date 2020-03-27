39fb886dfe3100a9174f9141dfadb55242c10af6
Irakli <i_iremashvili@cu.edu.ge>
2020-03-27 17:01:08 +0400

    rename twodio-social-media-front to front

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'twodio-social-media-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('twodio-social-media-front');
+    expect(app.title).toEqual('front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('twodio-social-media-front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
   });
 });
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'twodio-social-media-front';
+  title = 'front';
 }
