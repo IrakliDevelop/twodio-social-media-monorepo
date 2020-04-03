f641fd2b0aab6f6d6fc5b31a0cde4f9bc6c2f9bb
Zurab Benashvili <zura.benashvili@gmail.com>
2020-04-03 12:15:52 +0400

    substitute 'body-parser' with express builtin middlewares.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,10 @@
 import express, { Express } from 'express';
-import bodyParser from 'body-parser';
 
 import config from './config';
 
 const app: Express = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 if (!config.isLambda) {
   app.listen(config.server.port, () => console.log(`Api listening on port ${config.server.port}`));
