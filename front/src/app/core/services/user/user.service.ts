7c720170200daa0f7c1892f6c3264b9450c03c30
Irakli <i_iremashvili@cu.edu.ge>
2020-04-10 20:35:01 +0400

    add fetch user data method to user service

--- front/src/app/core/services/user/user.service.ts
+++ front/src/app/core/services/user/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private readonly URL: string = environment.ApiUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // TODO: maybe rename it?
+  fetchUserData(): Observable<any> {
+    return this.http.get<any>(`${this.URL}/api/me`);
+  }
 }
