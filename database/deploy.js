eaead01487683cec335b83c5a7cb9a4ac874fc5e
Zurab Benashvili <zura.benashvili@gmail.com>
2020-04-21 16:23:28 +0400

    refactor database deploy script

--- database/deploy.js
+++ database/deploy.js
@@ -1,36 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
+const colors = require('colors');
 const superagent = require('superagent');
 
 const taskPrefix = 'Deploy:dgraph-schema -';
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
-const log = (type, ...msgs) => {
-  console[type](taskPrefix, ...msgs);
-};
+const defaultEnv = 'dev';
+const defaultLog = () => {};
+const defaultDelayMs = 2000;
 
-async function deploy (databaseUrl) {
+async function deploy (
+  databaseUrl,
+  log = defaultLog,
+  delayMs = defaultDelayMs
+) {
+  await delay(delayMs);
   const schemaPath = path.resolve(__dirname, './schema.gql');
-  for (let i = 0; i < 3; ++i) {
-    try {
-      log('log', `deploying to url: ${databaseUrl}`);
-      await superagent
-        .post(url.resolve(databaseUrl, 'admin/schema'))
-        .send(fs.readFileSync(schemaPath, 'utf8'));
-      break;
-    } catch (e) {
-      log('error', e);
-      await delay(2000);
-    }
+  log('log', `deploying to url: ${databaseUrl}`);
+  const result = await superagent
+    .post(url.resolve(databaseUrl, 'admin/schema'))
+    .send(fs.readFileSync(schemaPath, 'utf8'));
+
+  const errors = result.body && result.body.errors || [];
+  if (errors.length) {
+    throw Error(errors.map(x => x.message || x));
   }
 };
 
 module.exports.deploy = deploy;
-module.exports.deployToEnv = function (env) {
+module.exports.deployToEnv = function (
+  env = defaultEnv,
+  log = defaultLog,
+  delayMs = defaultDelayMs
+) {
   switch (env) {
     case 'dev':
-      return deploy('http://localhost:8070');
+      return deploy('http://localhost:8070', log, delayMs);
     default:
       throw Error(`${taskPrefix} unknown env: ${env}`);
   }
--- tasksfile.js
+++ tasksfile.js
@@ -1,24 +1,57 @@
 const fs = require('fs');
 const { sh, cli, help } = require('tasksfile');
 const YAML = require('yaml');
+const colors = require('colors');
 
 const codegenConfig = YAML.parse(fs.readFileSync('./codegen.yml', 'utf8'));
+
+const typeColor = (type) => {
+  switch (type) {
+    case 'success': return colors.bgBlack.green;
+    case 'warn': return colors.yellow;
+    case 'error': return colors.bgBlack.red;
+    default: return x => x;
+  }
+};
+
+const logger = prefix => (type, ...msgs) => {
+  const msgColor = typeColor(type);
+  type = type === 'success' ? 'log' : type;
+  const msg = msgs.length > 1 ? JSON.stringify(msgs, 0, 2) : msgs.join(' ');
+  console[type](
+    prefix.bold,
+    msgColor(msg)
+  );
+};
 
 function rebuildTypes(options) {
   sh('npx graphql-codegen --config ./codegen.yml');
 }
 
-function deployDatabase(options, env) {
-  return require('./database/deploy').deployToEnv(env || options.env || 'dev');
+async function deployDatabase(options, env) {
+  const log = logger('Deploy:dgraph-schema -');
+  const deployToEnv = require('./database/deploy').deployToEnv
+  try {
+    await deployToEnv(env || options.env, log);
+  } catch (err) {
+    log('error', err.message || err);
+    throw err;
+  }
 }
 
 async function deploy(options, ...objects) {
+  const log = logger('Deploy -');
   options.env = options.env || 'dev';
   const hasObject = obj => objects.length <= 0 || objects.includes(obj);
-  if (['database', 'db'].some(obj => hasObject(obj))) {
-    await deployDatabase(options, options.env);
+
+  try {
+    if (['database', 'db'].some(obj => hasObject(obj))) {
+      await deployDatabase(options, options.env);
+    }
+    log('success', 'Success!');
+  } catch (_) {
+    log('error', 'Fail!');
   }
-  console.log('Deployments finished!');
 }
 
 help(rebuildTypes, `rebuild types generated from GraphQL schema: ${codegenConfig.schema}`);
