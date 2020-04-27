5c886dafa399dcee5cb4a6666316d5b090f8f643
Irakli <i_iremashvili@cu.edu.ge>
2020-04-27 18:54:05 +0400

    add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import './database';
 import express, { Express } from 'express';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 
 import config from './config';
@@ -10,6 +11,7 @@ import { apiRouter } from './routes';
 const app: Express = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.get('/', (_, res) => res.json({ ok: true }));
 app.use('/api', apiRouter());
 app.use(errorHandler());
