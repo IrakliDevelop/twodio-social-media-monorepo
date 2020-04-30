43c84fed622e98e39802a0cf55dc0a0618960dbb
Irakli <i_iremashvili@cu.edu.ge>
2020-04-30 17:45:58 +0400

    add post creating

--- front/src/app/modules/home/feed/feed.component.ts
+++ front/src/app/modules/home/feed/feed.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {finalize, takeUntil} from 'rxjs/operators';
 import {PostsService} from '@core/services';
 import {IPost} from '@core/models';
+import {Subject} from 'rxjs';
+
 
 @Component({
   selector: 'app-feed',
@@ -11,21 +15,50 @@ export class FeedComponent implements OnInit {
   limit = 20;
   offset = 0;
   after: string;
+
   posts: IPost[];
+  post: IPost;
+  postForm: FormGroup;
+
+  unsubscribe$: Subject<void>;
+  postLoading: boolean;
 
   constructor(
-    private postsService: PostsService
-  ) { }
+    private postsService: PostsService,
+    private fb: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
+    this.unsubscribe$ = new Subject<void>();
     this.posts = [];
+    this.postForm = this.fb.group({
+      text: new FormControl('', [Validators.required]),
+    });
     this.loadPosts(this.limit, this.offset);
   }
+
   loadPosts(limit: number, offset: number, after?: string) {
     this.postsService.getPosts(limit, offset, after).subscribe(res => {
       console.log(res);
       this.posts = res;
     });
+  }
+
+  addNewPost(): any {
+    if (this.postForm.invalid) {
+      return;
+    }
+    this.post = {text: this.postForm.value};
+    console.log(this.post);
+    this.postLoading = true;
+    this.postsService.createPost(this.post)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        finalize(
+          () => this.postLoading = false
+          )
+      ).subscribe(res => console.log(res));
   }
 
 }
--- front/src/app/modules/home/home.module.ts
+++ front/src/app/modules/home/home.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from '@shared/shared.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeedComponent } from './feed/feed.component';
-
 
 @NgModule({
   declarations: [DashboardComponent, FeedComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    SharedModule,
   ],
 })
 export class HomeModule { }
