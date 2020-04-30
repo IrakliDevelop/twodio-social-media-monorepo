c4af2f4fe7ece17ea37f014c1ba76d6ed8432ec5
Irakli <i_iremashvili@cu.edu.ge>
2020-04-30 17:24:50 +0400

    create posts service

---
+++ front/src/app/core/services/posts/posts.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  URL: string = environment.ApiUrl;
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getPosts(first?: number, offset?: number, after?: string): Observable<any> {
+    return this.http.get<any>(`${this.URL}/api/posts`, {
+      params: {
+        first: first.toString(),
+        offset: offset.toString(),
+        after,
+      },
+    });
+  }
+  createPost(post: any): Observable<any> {
+    return this.http.post(`${this.URL}/api/posts`, post);
+  }
+}
