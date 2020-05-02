17389e26f8f715f3c9aac1e462c7fffdd47e0a4c
Zurab Benashvili <zura.benashvili@gmail.com>
2020-05-02 10:12:11 +0400

    feat(api): add user search route

--- api/src/routes/user/index.ts
+++ api/src/routes/user/index.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { UserModel } from '../../models/user';
+import { UserModel, Query } from '../../models';
 
 export const userRouter = () => {
   const userModel = container.resolve(UserModel);
@@ -23,6 +23,28 @@ export const userRouter = () => {
     );
 
     res.json({ ok: true });
+  });
+
+  router.get('/search/:term/:after?', async (req: Request, res: Response) => {
+    const query = (name: string) => new Query('user', name)
+      .first(10)
+      .after(req.params.after)
+      .vars({ term: ['string', req.params.term] })
+      .project({
+        id: 1,
+        fullName: 1,
+        username: 1,
+        followsCount: 'count(User.follows)',
+        followersCount: 'count(User.followers)',
+        postsCount: 'count(User.posts)',
+      });
+
+    res.json(await userModel.runQueries(
+      query('byUsername')
+        .func('match(User.username, $term, 3)'),
+      query('byFullName')
+        .func('match(User.fullName, $term, 16)')
+    ).then(x => x.getJson()));
   });
 
   return router;
