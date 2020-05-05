4d5acd8c89352925dc41ae8c9cdaea3bc25d41bc
Zurab Benashvili <zura.benashvili@gmail.com>
2020-05-05 20:09:13 +0400

    BREAKING CHANGE: move .env from api to project root

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 import { config } from 'dotenv';
 
-config({ path: resolve(__dirname, '../../.env' )});
+config({ path: resolve(__dirname, '../../../.env' )});
 
 const root = resolve(__dirname, '../../');
 const env = process.env;
