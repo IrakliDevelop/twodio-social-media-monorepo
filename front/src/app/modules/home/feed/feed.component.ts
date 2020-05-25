8b0b3e17df9ec5dadb6b494c6745b30439105a41
irakli <i_iremashvili@cu.edu.ge>
2020-05-25 14:17:24 +0400

    (front) format post date and reset post field after creation

--- front/src/app/modules/home/feed/feed.component.ts
+++ front/src/app/modules/home/feed/feed.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {finalize, takeUntil} from 'rxjs/operators';
+import * as moment from 'moment';
 import {PostsService, WsService} from '@core/services';
 import {IPost} from '@core/models';
 import {Subject} from 'rxjs';
@@ -48,9 +49,11 @@ export class FeedComponent implements OnInit {
     this.postsService.getPosts(limit, offset, after).pipe(
       takeUntil(this.unsubscribe$),
       finalize(() => this.allPostsLoading = false)
-    ).subscribe(res => {
-      console.log(res);
-      this.posts = res;
+    ).subscribe((res: IPost[]) => {
+      this.posts = res.map(post => {
+        post.created = moment(post.created).fromNow();
+        return post;
+      });
     });
   }
 
@@ -68,6 +71,7 @@ export class FeedComponent implements OnInit {
           () => this.singlePostLoading = false
           )
       ).subscribe(res => {
+        this.postForm.reset();
         if (res.text && res.text === this.post.text) { // TODO: this is temporarily. use proper success checking
           this.loadPosts(this.limit, this.offset);
         }
