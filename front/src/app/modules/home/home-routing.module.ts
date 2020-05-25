6d2dfaa0f71e8dea5c3b12b59b1fa77b45136cee
irakli <i_iremashvili@cu.edu.ge>
2020-05-25 14:26:26 +0400

    (front) add title to home routes

--- front/src/app/modules/home/home-routing.module.ts
+++ front/src/app/modules/home/home-routing.module.ts
@@ -8,10 +8,12 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    data: { title: 'home' },
   },
   {
     path: 'feed',
     component: FeedComponent,
+    data: { title: 'feed' },
   },
 ];
 
