dcc79b0abe31f306b05c1f0a4e095838c5b798cb
Zura Benashvili <zura.benashvili@gmail.com>
2020-06-16 01:27:38 +0400

    feat(front): implement chat service

--- front/src/app/core/services/index.ts
+++ front/src/app/core/services/index.ts
@@ -2,4 +2,5 @@ export * from './auth/auth.service';
 export * from './ws/ws.service';
 export * from './user/user.service';
 export * from './posts/posts.service';
+export * from './chat/chat.service';
 export * from './ws/ws.service';
